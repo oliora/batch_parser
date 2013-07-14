d3dd9cbbbb3edb4a547ca7564bf1d5a4f5be44f3
Andrey Upadyshev <oliora@gmail.com>
2013-07-14 12:35:23 +0400

    Initial Commit

---
+++ batch_parser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  batch_parser
+//
+//  Created by Andrey Upadyshev on 14.07.13.
+//  Copyright (c) 2013 Andrey Upadyshev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
