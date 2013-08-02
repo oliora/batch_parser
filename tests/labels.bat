8a69dc9bd0d4e979f7c01c41f722299a2963f692
Andrey Upadyshev <upadan@f-secure.com>
2013-08-02 20:30:06 +0400

    Labels and redirects samples extended

--- tests/labels.bat
+++ tests/labels.bat
@@ -2,8 +2,13 @@
 
 goto "1^echo
 goto :EOF
+c:lbl1
+bla
 
-(
+>r1 :lbl
+echo wow
+
+(((
 echo 12 &&  &&
 : "1^^echo 23 44
 echo label 12
--- tests/redirect.bat
+++ tests/redirect.bat
@@ -1,4 +1,6 @@
 echo 1 > r1
 > f1 :ech 2
 echo 3 >r3 2>r4
-(echo 1)>> r1 2>>r4
+echo 15 ^>f1
+echo 15 ^>^>f1
+(echo wow) 2>>r4 >>r1
